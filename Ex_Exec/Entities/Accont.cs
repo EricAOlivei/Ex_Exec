981736c17f84cfb21eab39d3224c6cea18e3a838
EricAOlivei <oliveira_eric@educadventista.org>
2022-03-08 10:51:00 -0300

    Add project files.

---
+++ Ex_Exec/Entities/Accont.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ex_Exec.Entities.Exceptions;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_Exec.Entities
+{
+    internal class Accont
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        public Accont()
+        {
+        }
+
+        public Accont(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount  ;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (WithDrawLimit < amount)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+            if (Balance < amount)
+            {
+                throw new DomainException("Not enough balance");
+            }
+
+            Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return "New balance: " + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Ex_Exec/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Ex_Exec.Entities;
+using Ex_Exec.Entities.Exceptions;
+using System.Globalization;
+
+namespace Ex_Exec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter account data");
+            Console.WriteLine();
+
+            Console.Write("Number: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Holder: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Withdraw limit: ");
+            double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Accont accont = new Accont(id, name, balance, limit);
+
+            Console.WriteLine();
+
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            try
+            {
+                accont.Withdraw(amount);
+                Console.WriteLine(accont);
+            }
+
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}   
